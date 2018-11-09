893ad5f7ccb3adbbe89690852d85a2e8c4ddf8b1
paramagician <konnor5456@gmail.com>
2018-11-09 13:54:18 +0000

    Modified layout, creating CLI for rest request

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -2,12 +2,80 @@
 
 require 'rest-client'
 
-url = 'localhost:3000/users/'
+URL = 'localhost:3000/users/'
+INPUTS = {
+  get: {
+    show: 'show/5',
+    edit: '5/edit',
+    new: 'new',
+    index: ''
+  },
 
-loop do
+  patch: {
+    patch_update: '15'
+  },
+
+  put: {
+    put_update: '20'
+  },
+
+  delete: {
+    destroy: '25'
+  }
+}.freeze
+
+def greeting
+  puts 'Valid requests are:'
+
+  p INPUTS.map { |_key, value| value.keys }.flatten
+  puts 'type exit to exit the loop'
   puts 'Enter your request'
-
-  input = gets.chomp
-  puts RestClient.get(url + input)
-  puts url + input
 end
+
+def cli_input
+  input = gets.chomp.to_sym
+
+  return nil if input.nil?
+  return :exit if input == :exit
+
+  INPUTS.each do |_key, hash|
+    return nil unless hash.key?(input)
+  end
+  input
+end
+
+def method_url_logic(input:)
+  INPUTS.each do |method, hash|
+    next unless hash.key?(input)
+
+    @method = method if hash.key?(input)
+
+    hash.each do |key, value|
+      next unless key == input
+
+      @url_ext = value if key == input
+    end
+  end
+end
+
+def cli_loop
+  loop do
+    greeting
+    input = cli_input
+
+    p input
+
+    exit if input == :exit
+    next if input.nil?
+
+    method_url_logic(input: input)
+
+    url = URL + @url_ext
+
+    puts RestClient.send(@method, url)
+    puts @method
+    puts url
+  end
+end
+
+cli_loop
