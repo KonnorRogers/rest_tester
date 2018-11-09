83d962b288aeb0e08e77519539d2057e9c32af90
paramagician <konnor5456@gmail.com>
2018-11-09 03:09:34 +0000

    Added all routes

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -2,5 +2,12 @@
 
 require 'rest-client'
 
-url = 'localhost:3000'
-puts RestClient.get(url)
+url = 'localhost:3000/users/'
+
+loop do
+  puts 'Enter your request'
+
+  input = gets.chomp
+  puts RestClient.get(url + input)
+  puts url + input
+end
